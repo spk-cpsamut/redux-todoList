3499dcd230fe458268797f51d456ce371bc2ed5d
spk-cpsamut <taresupakon@gmail.com>
2020-05-14 17:52:50 +0700

    put thunk

--- src/index.js
+++ src/index.js
@@ -5,17 +5,44 @@ import "./css/todo.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 
 import userReducer from "./store/reducer/user";
 import todoListReducer from "./store/reducer/todoList";
+
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
   user: userReducer,
   todo: todoListReducer,
 });
 
-const store = createStore(rootReducer);
+const logger1 = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log("[Middleware]1 dispatching", action);
+      next(action);
+      console.log("1---->", store.getState());
+    };
+  };
+};
+
+const logger2 = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log("[Middleware]2 dispatching", action);
+      next(action);
+      console.log("2---->", store.getState());
+    };
+  };
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger1, thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
